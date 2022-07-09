440f3d60312ddd50b51dbcd3a9b31f24895a1305
Dhruv Gursahani <dhruv.gursahani@outlook.com>
2022-07-09 16:30:12 +0530

    fix: :bug: chnaged bg color of safe area

--- utils/SafeViewAndroid.js
+++ utils/SafeViewAndroid.js
@@ -3,7 +3,7 @@ import { StyleSheet, Platform, StatusBar } from 'react-native'
 export default StyleSheet.create({
   AndroidSafeArea: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: '#eee',
     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
   }
 })

333830e3f90388a35eb4c3cf4ccdf2e68cbbc9e4
Dhruv Gursahani <dhruv.gursahani@outlook.com>
2022-07-09 13:06:57 +0530

    feat: :sparkles: header UI

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar'
 import { TailwindProvider } from 'tailwindcss-react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import HomeScreen from './screens/HomeScreen'
 
 const Stack = createNativeStackNavigator()
 
@@ -9,7 +10,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <TailwindProvider>
-        <Stack.Navigator>{/* Screens */}</Stack.Navigator>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
       </TailwindProvider>
     </NavigationContainer>
   )
---
+++ screens/HomeScreen.js
@@ -0,0 +1,68 @@
+import { useLayoutEffect } from 'react'
+import {
+  Text,
+  SafeAreaView,
+  View,
+  Image,
+  TextInput,
+  ScrollView
+} from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import SafeViewAndroid from '../utils/SafeViewAndroid'
+import {
+  UserIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  AdjustmentsIcon
+} from 'react-native-heroicons/outline'
+
+const HomeScreen = () => {
+  const navigation = useNavigation()
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false
+    })
+  }, [])
+
+  return (
+    <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
+      {/* Header */}
+      <View className="flex-row p-3 items-center mx-4 space-x-2">
+        <Image
+          source={{ uri: 'https://links.papareact.com/wru' }}
+          className="h-7 w-7 bg-gray-300 rounded-full"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+        <UserIcon size={25} color="#00CCBB" />
+      </View>
+
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 mx-4 pb-2">
+        <View className="flex-row spzce-x-2 flex-1 bg-gray-200 p-3 items-center">
+          <SearchIcon color="gray" size={20} />
+          <TextInput
+            placeholder="Restaurants and Cuisines"
+            keyboardType="default"
+            className="ml-1"
+          />
+        </View>
+        <AdjustmentsIcon color="#00CCBB" />
+      </View>
+
+      {/* Body */}
+      <ScrollView>
+        {/* Categories */}
+        {/* Featured row */}
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
